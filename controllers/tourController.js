94b2848cfa614d94df264115bd809b9bca906668
Rust401 <zhaodonghu94@gmail.com>
2020-04-04 20:36:53 +0800

    matching and grouping

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -144,3 +144,47 @@ exports.addTour = async (req, res) => {
         }
     ); */
 };
+
+exports.getTourStats = async (req, res) => {
+    try {
+        const stats = await Tour.aggregate([
+            {
+                $match: { ratingsAverage: { $gte: 4.5 } }
+            },
+            {
+                $group: {
+                    //_id: '$ratingsAverage',
+                    _id: { $toUpper: '$difficulty' },
+                    num: { $sum: 1 },
+                    numRatings: { $sum: '$ratingsQuantity' },
+                    avgRating: { $avg: '$ratingsAverage' },
+                    avgPrice: { $avg: '$price' },
+                    minPrice: { $min: '$price' },
+                    maxPrice: { $max: '$price' }
+                }
+            },
+            {
+                $sort: {
+                    avgPrice: 1
+                }
+            }
+            /* {
+                $match: {
+                    _id: { $ne: 'EASY' }
+                }
+            } */
+        ]);
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                stats
+            }
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: 'Invalid data sent'
+        });
+    }
+};
