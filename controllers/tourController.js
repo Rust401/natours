c244556351be9948a291c17ed0dfbcf5c97e07c3
Rust401 <zhaodonghu94@gmail.com>
2020-04-04 15:08:01 +0800

    add refractoring

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,6 @@
 /* const fs = require('fs'); */
 const Tour = require('../models/tourModules');
+const APIFeatures = require('../utils/apiFeatures');
 
 /* const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
@@ -25,38 +26,22 @@ const Tour = require('../models/tourModules');
     next();
 }; */
 
+exports.aliasTopTours = (req, res, next) => {
+    req.query.limit = '5';
+    req.query.sort = '-ratingsAverage,summary,difficulty';
+    req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+    next();
+};
+
 exports.getAllTours = async (req, res) => {
     try {
-        //BUILD QUERY
-        // 1A) Filtering
-        const queryObj = { ...req.query };
-        const excludeFields = ['page', 'sort', 'limit', 'fields'];
-        excludeFields.forEach(el => delete queryObj[el]);
-
-        // 1B) Advanced filtering
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(
-            /\b(gte|gt|lte|lt)\b/g,
-            match => `$${match}`
-        );
-        console.log(JSON.parse(queryStr));
-
-        let query = Tour.find(JSON.parse(queryStr));
-
-        // 2) Sorting
-        if (req.query.sort) {
-            query = query.sort(req.query.sort);
-        }
-
-        //{difficulty: 'easy',duration: {$gte:5}}
-        /* const query = Tour.find()
-            .where('duration')
-            .equals(5)
-            .where('difficulty')
-            .equals('easy'); */
-
         //EXECUTE QUERY
-        const tours = await query;
+        const features = new APIFeatures(Tour.find(), req.query)
+            .filter()
+            .sort()
+            .limitFields()
+            .paginate();
+        const tours = await features.query;
 
         //SEND RESPONSE
         res.status(200).json({
