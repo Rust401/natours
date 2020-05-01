9157da89d2349ca465638b047d07f5096e7a536b
Rust401 <zhaodonghu94@gmail.com>
2020-05-01 18:47:06 +0800

    login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,20 @@
-exports.getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: 'fail',
-        message: 'router not define yet'
+const User = require('../models/userModules');
+const APIFeatures = require('../utils/apiFeatures');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+    const users = await User.find();
+
+    //SEND RESPONSE
+    res.status(200).json({
+        status: 'success',
+        results: users.length,
+        data: {
+            users
+        }
     });
-};
+});
 
 exports.addUser = (req, res) => {
     res.status(500).json({
