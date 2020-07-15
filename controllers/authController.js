4afa48e6a69632d9d3e396c1c107f92fc0414a6e
Rust401 <zhaodonghu94@gmail.com>
2020-07-15 23:26:24 +0800

    add role

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,13 +12,14 @@ const signToken = id => {
 };
 
 exports.signup = catchAsync(async (req, res, next) => {
-    const newUser = await User.create({
+    /* const newUser = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
         passwordConfirm: req.body.passwordConfirm,
         passwordChangedAt: req.body.passwordChangedAt
-    });
+    }); */
+    const newUser = await User.create(req.body);
     console.log(newUser);
 
     const token = signToken(newUser._id);
