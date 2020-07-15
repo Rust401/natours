9c0607e69400c386e723404702170f0cfea6eed1
Rust401 <zhaodonghu94@gmail.com>
2020-07-15 15:52:26 +0800

    add protect about whether login

--- controllers/authController.js
+++ controllers/authController.js
@@ -50,3 +50,29 @@ exports.login = catchAsync(async (req, res, next) => {
         token
     });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+    let token;
+    // 1) Getting token and check of it's there
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')
+    ) {
+        token = req.headers.authorization.split(' ')[1];
+    }
+    console.log(token);
+
+    if (!token) {
+        return next(
+            new AppError(
+                'You are not logged in! Please log in to get acces.',
+                401
+            )
+        );
+    }
+
+    // 2) Verification token
+    // 3) Chekc if user still exists
+    // 4) Check if user changed passwd after the token was issued
+    next();
+});
